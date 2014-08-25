9d010888d02c01732d1460f9d93635fa7df14dbd
Diego Caro <diegocaro@gmail.com>
2014-08-25 13:32:33 -0400

    Updating scripts

--- space.sh
+++ space.sh
@@ -9,20 +9,24 @@ echo BitmapB $BITB
 echo Typegraph $TYPEGRAPH
 echo Reading $INPUT
 
-OUTFILE=$OUTPUT-2,2,0,0,0-$BITT-$BITB.mxd
-echo Creating $OUTFILE
-gzcat $INPUT | ./create -s MXD -t $BITT -b $BITB -g $TYPEGRAPH -f 2,2,0,0,0 $OUTFILE
+HEADER=`gzcat $INPUT|head -n1`
+LEVELS=`python -c "import math; h = '$HEADER'.split(); print int(math.ceil(math.log(max(int(h[0]),int(h[2]))/math.log(2) )));"`
 
-for lf in {1..32}
+for ((lf=1; lf <= $LEVELS; lf++ ))
 do
     OUTFILE=$OUTPUT-2,2,0,0,$lf-$BITT-$BITB.mxf
     echo Creating $OUTFILE
     gzcat $INPUT | ./create -s MXF -t $BITT -b $BITB -g $TYPEGRAPH -f 2,2,0,0,$lf $OUTFILE
 done
 
-for l in {1..32}
+for ((l=0; l <= $LEVELS; l++ ))
 do
     lki=$((l*2))
+    
+    OUTFILE=$OUTPUT-2,2,0,$lki,0-$BITT-$BITB.mxd
+    echo Creating $OUTFILE
+    gzcat $INPUT | ./create -s MXD -t $BITT -b $BITB -g $TYPEGRAPH -f 2,2,0,$lki,0 $OUTFILE
+    
     OUTFILE=$OUTPUT-2,2,0,$lki,0-$BITT-$BITB.prb
     echo Creating $OUTFILE
     gzcat $INPUT | ./create -s PRB -t $BITT -b $BITB -g $TYPEGRAPH -f 2,2,0,$lki,0 $OUTFILE
