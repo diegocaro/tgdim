44025144fe3e85c14638bde56989971cb1604c2a
Diego Caro <diegocaro@gmail.com>
2014-09-15 11:15:38 -0300

    Creating structures with even levels... thats enough

--- space-echo.sh
+++ space-echo.sh
@@ -12,26 +12,26 @@ echo Reading $INPUT
 HEADER=`gzcat $INPUT|head -n1`
 LEVELS=`python -c "import math; h = '$HEADER'.split(); print int(math.ceil(math.log(max(int(h[0]),int(h[2]))/math.log(2) )));"`
 
-for ((lf=1; lf <= $LEVELS; lf++ ))
+for ((lf=2; lf <= $LEVELS; lf+=2 ))
 do
     OUTFILE=$OUTPUT-2,2,0,0,$lf-$BITT-$BITB.mxf
-    echo Creating $OUTFILE
+    echo "echo Creating $OUTFILE"
     echo "gzcat $INPUT | ./create -s MXF -t $BITT -b $BITB -g $TYPEGRAPH -f 2,2,0,0,$lf $OUTFILE"
 done
 
-for ((l=0; l <= $LEVELS; l++ ))
+for ((l=0; l <= $LEVELS; l+=2 ))
 do
     lki=$((l*2))
     
     OUTFILE=$OUTPUT-2,2,0,$lki,0-$BITT-$BITB.mxd
-    echo Creating $OUTFILE
+    echo echo "Creating $OUTFILE"
     echo "gzcat $INPUT | ./create -s MXD -t $BITT -b $BITB -g $TYPEGRAPH -f 2,2,0,$lki,0 $OUTFILE"
     
     OUTFILE=$OUTPUT-2,2,0,$lki,0-$BITT-$BITB.prb
-    echo Creating $OUTFILE
+    echo echo "Creating $OUTFILE"
     echo "gzcat $INPUT | ./create -s PRB -t $BITT -b $BITB -g $TYPEGRAPH -f 2,2,0,$lki,0 $OUTFILE"
     
     OUTFILE=$OUTPUT-2,2,0,$lki,0-$BITT-$BITB.prw
-    echo Creating $OUTFILE
+    echo echo "Creating $OUTFILE"
     echo "gzcat $INPUT | ./create -s PRW -t $BITT -b $BITB -g $TYPEGRAPH -f 2,2,0,$lki,0 $OUTFILE"
 done
